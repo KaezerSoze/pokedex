057529598fbb8821dbc24bc5c3453a850f383e9f
kaezersoze <fingerzjbm@gmail.com>
2023-03-30 11:23:22 +0200

    events.1 alerte

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import PokemonCard from './components/PokemonCard';
 import NavBar from './components/NavBar';
@@ -8,52 +8,85 @@ function App() {
     {
       name: "bulbasaur",
       imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+     
+      color: "#78C850"
     },
     {
       name: "salamender",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      
+      color: "#F08030"
     },
     {
       name: "squirtle",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      
+      color: "#6890F0"
     },
     {
       name: "pikachu",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      
+      color:"#F7D346"
+
     },
     {
-      name: "new"
+      name: "new",
+      color: "#000000"
     }
   ];
   const [pokemonIndex, setPokemonIndex] = useState(0);
+  const [pokemonObject, setPokemonObject] = useState(pokemonList[pokemonIndex])
 
+    
+    
   const handleNextPokemon = () => {
     if (pokemonIndex < pokemonList.length - 1) {
       setPokemonIndex(pokemonIndex + 1);
+      setPokemonObject(pokemonList[pokemonIndex])
+       if(
+        pokemonIndex === 3 
+      ) { alert("pika picachu")}
     }
-  };
+
+    }
+  
 
   const handlePreviousPokemon = () => {
     if (pokemonIndex > 0) {
       setPokemonIndex(pokemonIndex - 1);
+      setPokemonObject(pokemonList[pokemonIndex])
+      if(
+        pokemonIndex === 3 
+      ) { alert("pika picachu")}
     }
   };
 
   const pokemon = pokemonList[pokemonIndex];
 
+  useEffect(() => {
+    alert("hello pokemon trainer :)");
+  }, []);
+
+  
+  
+
   return (
     <div>
-      <h1>PoKeDex</h1>
       <NavBar
         handleNextPokemon={handleNextPokemon}
         handlePreviousPokemon={handlePreviousPokemon}
         pokemonIndex={pokemonIndex}
-        pokemonListLength={pokemonList.length}
-      />
-      <PokemonCard pokemon={pokemon} />
+        pokemonListLength={pokemonList.length}/>
+          <div className="pokemon-card-container">
+        <h1 style={{ color: pokemon.color }}>PoKédex</h1>
+        <PokemonCard pokemon={pokemon} />
+        
+     
+      </div>
     </div>
   );
-}
-
+  
+  }
 export default App;
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,30 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
 
 
 
 function NavBar({ handleNextPokemon, handlePreviousPokemon, pokemonIndex, pokemonListLength }) {
-  return (
-    <div>
-      {pokemonIndex > 0 && <button onClick={handlePreviousPokemon}>Précédent</button>}
-      {pokemonIndex < pokemonListLength - 1 && <button onClick={handleNextPokemon}>Suivant</button>}
-    </div>
-  );
-}
+    return (
+        
+      <div style={{ color: "#2a75bb" }}>
+        {pokemonIndex > 0 && <button onClick={handlePreviousPokemon}>Précédent</button>}
+        {pokemonIndex < pokemonListLength - 1 && <button onClick={handleNextPokemon}>Suivant</button>}
+        {}
+
+      
+      </div>
+    );
+  }
+  
 
 NavBar.propTypes = {
   handleNextPokemon: PropTypes.func.isRequired,
   handlePreviousPokemon: PropTypes.func.isRequired,
   pokemonIndex: PropTypes.number.isRequired,
   pokemonListLength: PropTypes.number.isRequired,
+  onPokemonChange: PropTypes.func.isRequired,
 };
+
+
 
 export default NavBar;
 
