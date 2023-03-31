602f0f522c56b35510c098de4aca19e7e6d38918
kaezersoze <fingerzjbm@gmail.com>
2023-03-31 11:17:15 +0200

    rendering-list.1 boutton par pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,34 @@
-import { useState } from 'react';
-import './App.css';
-import PokemonCard from './components/PokemonCard';
-import NavBar from './components/NavBar';
+import React, { useState } from "react";
+import PokemonCard from "./components/PokemonCard";
+import NavBar from "./components/NavBar";
 
-function App() {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "salamender",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
-    },
-    {
-      name: "squirtle",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
-    },
-    {
-      name: "pikachu",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-    },
-    {
-      name: "new"
-    }
-  ];
-  const [pokemonIndex, setPokemonIndex] = useState(0);
 
-  const handleNextPokemon = () => {
-    if (pokemonIndex < pokemonList.length - 1) {
-      setPokemonIndex(pokemonIndex + 1);
-    }
-  };
+function App()
+ {  const pokemonList = [ 
+     {      name: "bulbasaur",     
+            imgSrc:        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",   
+     },  
+       {      name: "charmander",   
+              imgSrc:        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",  
+        },   
+         {      name: "squirtle",   
+                imgSrc:        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",   
+         },  
+           {      name: "pikachu",   
+                  imgSrc:        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",   
+                 },    {   
+                  name: "mew",   
+                 }, 
+                 ];
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+  const handlePokemonClick = (index) => setpokemonIndex(index);
 
-  const handlePreviousPokemon = () => {
-    if (pokemonIndex > 0) {
-      setPokemonIndex(pokemonIndex - 1);
-    }
-  };
-
-  const pokemon = pokemonList[pokemonIndex];
-
-  return (
-    <div>
-      <h1>PoKeDex</h1>
-      <NavBar
-        handleNextPokemon={handleNextPokemon}
-        handlePreviousPokemon={handlePreviousPokemon}
-        pokemonIndex={pokemonIndex}
-        pokemonListLength={pokemonList.length}
-      />
-      <PokemonCard pokemon={pokemon} />
-    </div>
-  );
-}
-
+  return (   
+    <div>    
+      <NavBar pokemonList={pokemonList}
+       onPokemonClick={handlePokemonClick} />  
+       
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} /> 
+         </div> 
+   );}
 export default App;
-
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,16 @@
-import PropTypes from "prop-types";
-import { useState } from "react";
+import React from "react";
 
-
-
-function NavBar({ handleNextPokemon, handlePreviousPokemon, pokemonIndex, pokemonListLength }) {
-  return (
-    <div>
-      {pokemonIndex > 0 && <button onClick={handlePreviousPokemon}>Précédent</button>}
-      {pokemonIndex < pokemonListLength - 1 && <button onClick={handleNextPokemon}>Suivant</button>}
-    </div>
-  );
-}
-
-NavBar.propTypes = {
-  handleNextPokemon: PropTypes.func.isRequired,
-  handlePreviousPokemon: PropTypes.func.isRequired,
-  pokemonIndex: PropTypes.number.isRequired,
-  pokemonListLength: PropTypes.number.isRequired,
-};
-
+function NavBar(
+  { onPokemonClick, pokemonList }) 
+  {  return (  
+      <div>  <p>PoKeDex</p>    
+      {pokemonList.map((pokemon, index) => (       
+         <button key={index} onClick={() => onPokemonClick(index)}>  
+         {pokemon.name}        </button>    
+           ))}  
+             </div>  
+             );}
 export default NavBar;
-
-    
-    
 
 
 
